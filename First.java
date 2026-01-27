f7d00b8b88d7d424b798cb0acf718ab4114fbce4
Amber Edmond <ambersays@Ambers-MacBook-Pro.local>
2026-01-26 22:38:13 -0500

    Module 1, Assignment 1

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Software Developer");
+        System.out.println("Why: I enjoy using software to solve problems");
+        System.out.println("Median salary: $131,450");
+    }
+
+}
